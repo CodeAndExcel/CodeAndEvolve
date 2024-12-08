367e7f20e1a1d560fa1f35604182e30c710b166b
Yugesh <yugesh2247@gmail.com>
2024-12-08 22:20:18 +0530

    Initial commit

---
+++ src/main/java/com/code/evolve/CodeAndEvolveApplication.java
@@ -0,0 +1,13 @@
+package com.code.evolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAndEvolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeAndEvolveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/evolve/CodeAndEvolveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.evolve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeAndEvolveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
