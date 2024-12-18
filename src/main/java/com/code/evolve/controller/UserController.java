7c470e842399ede671b4bb3ec18db0e0861a0990
Yugesh <yugesh2247@gmail.com>
2024-12-18 23:07:40 +0530

    Basic Authorization

--- src/main/java/com/code/evolve/controller/UserController.java
+++ src/main/java/com/code/evolve/controller/UserController.java
@@ -2,10 +2,12 @@ package com.code.evolve.controller;
 
 import com.code.evolve.model.User;
 import com.code.evolve.service.UserService;
+import com.code.evolve.util.TokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -14,6 +16,9 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    TokenUtil tokenUtil;
 
     @PostMapping("/addUser")
     public ResponseEntity<?> addUser(@RequestBody User user){
@@ -28,6 +33,19 @@ public class UserController {
         }
         return ResponseEntity.ok(userDetails);
     }
+
+    @GetMapping("/getToken")
+    public String generateToken(){
+        return tokenUtil.generateToken();
+    }
+
+    @GetMapping("/validate")
+    public Date validate(){
+        String token=tokenUtil.generateToken();
+        return tokenUtil.extractClaims(token).getExpiration();
+    }
+
+
 
 
 
